c169e5241c3adde59cef596611e9dc03aed526d6
wengervictor329@gmail.com <wengervictor329@gmail.com>
2020-11-06 09:54:15 +0100

    exo55

--- exopartie5.5/assets/js/script.js
+++ exopartie5.5/assets/js/script.js
@@ -1,3 +1,19 @@
-function rose(){
-let pink = document.getElementById('p');
+function rose() {
+    document.getElementById('text').style.color = 'pink';
+}
+
+function bleu() {
+    document.getElementById('text').style.color = 'blue';
+}
+
+function violet() {
+    document.getElementById('text').style.color = 'purple';
+}
+
+function gras() {
+    document.getElementById('text').style.fontWeight = 'bold';
+}
+
+function italique() {
+    document.getElementById('text').style.fontStyle = 'italic';
 }
