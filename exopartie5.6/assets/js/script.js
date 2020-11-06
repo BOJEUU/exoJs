46501e7b3780fe8a92903e9967232871c19677e1
wengervictor329@gmail.com <wengervictor329@gmail.com>
2020-11-06 12:10:00 +0100

    exo6

--- exopartie5.6/assets/js/script.js
+++ exopartie5.6/assets/js/script.js
@@ -1,19 +1,4 @@
-function rose() {
-    document.getElementById('text').style.color = 'pink';
-}
-
-function bleu() {
-    document.getElementById('text').style.color = 'blue';
-}
-
-function violet() {
-    document.getElementById('text').style.color = 'purple';
-}
-
-function gras() {
-    document.getElementById('text').style.fontWeight = 'bold';
-}
-
-function italique() {
-    document.getElementById('text').style.fontStyle = 'italic';
+function bojeu();{
+    let bojeu = document.getElementById("Password");
+    bojeu.style.borderColor("black");
 }
